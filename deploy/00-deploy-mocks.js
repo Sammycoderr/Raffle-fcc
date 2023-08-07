0552a060a4de3d455eb47bd88cf903ba36a03323
sammycoderr <abiswags@gmail.com>
2023-08-07 18:31:10 +0100

    utils removed as parsether was no longer under utils, ethers upgraded back to version 6.7.0

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,7 +1,7 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 
-const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium cost. it costs 0.25 LINK
+const BASE_FEE = ethers.parseEther("0.25") // 0.25 is the premium cost. it costs 0.25 LINK
 const GAS_PRICE_LINK = 1e9 // link per gas: calculated value based on the gas price of the chain
 
 //Chainlink Nodes pay the gas fees tp give us randomness and do external execution
